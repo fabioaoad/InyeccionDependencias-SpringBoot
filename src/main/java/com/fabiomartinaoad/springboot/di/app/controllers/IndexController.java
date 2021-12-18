fcfae5bbf13c4bc3f4f2054ec4499f4b7cdd0a4e
Fabio Martin Aoad <fabioaoad@gmail.com>
2021-12-18 20:20:16 -0300

    Se añade la clase de servicio  MiServicio.java - S03A02

--- src/main/java/com/fabiomartinaoad/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/fabiomartinaoad/springboot/di/app/controllers/IndexController.java
@@ -1,14 +1,17 @@
 package com.fabiomartinaoad.springboot.di.app.controllers;
 
+import com.fabiomartinaoad.springboot.di.app.models.service.MiServicio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    private MiServicio servicio = new MiServicio();
 
     @GetMapping({"/", "","/index"})
     public String index(Model model){
+        model.addAttribute("objeto", servicio.operacion());
         return "index";
     }
 }
