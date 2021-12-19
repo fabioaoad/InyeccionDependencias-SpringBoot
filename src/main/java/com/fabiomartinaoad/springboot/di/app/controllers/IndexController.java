1f8f3249c99ced657b08f9941f3c73a8426b7698
Fabio Martin Aoad <fabioaoad@gmail.com>
2021-12-19 09:25:47 -0300

    Implementando inyección de dependencia con la anotación @Autowired - S03A03

--- src/main/java/com/fabiomartinaoad/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/fabiomartinaoad/springboot/di/app/controllers/IndexController.java
@@ -1,13 +1,19 @@
 package com.fabiomartinaoad.springboot.di.app.controllers;
 
 import com.fabiomartinaoad.springboot.di.app.models.service.MiServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    private MiServicio servicio = new MiServicio();
+    //Sin inyección de dependencia
+    //private MiServicio servicio = new MiServicio();
+
+    //Con inyección de dependencia. Se añade anotacion @Autowired
+    @Autowired
+    private MiServicio servicio;
 
     @GetMapping({"/", "","/index"})
     public String index(Model model){
--- src/main/java/com/fabiomartinaoad/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/fabiomartinaoad/springboot/di/app/models/service/MiServicio.java
@@ -1,7 +1,9 @@
 package com.fabiomartinaoad.springboot.di.app.models.service;
 
 
+import org.springframework.stereotype.Component;
 
+@Component
 public class MiServicio {
     public String operacion(){
         return "Ejecutando algún proceso...";
